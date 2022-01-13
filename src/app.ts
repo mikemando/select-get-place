90c4601ecf37b37fc4b902c28e19ba3ff85e3b54
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-01-12 23:47:58 -0800

    access form input value

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const formElement = document.querySelector("form")! as HTMLFormElement;
+const formInput = document.getElementById("address")! as HTMLInputElement;
+
+const formSubmitHandler = (event: Event) => {
+    event.preventDefault();
+    const input = formInput.value;
+};
+
+formElement.addEventListener("submit", formSubmitHandler);
