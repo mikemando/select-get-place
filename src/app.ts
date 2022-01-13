62efe0fb8d8470599461ec13919c0ef2be73fcb7
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-01-13 02:11:32 -0800

    get google api location results

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,35 @@
+import axios from "axios";
+
 const formElement = document.querySelector("form")! as HTMLFormElement;
 const formInput = document.getElementById("address")! as HTMLInputElement;
+
+const API_KEY = "";
+
+type GoogleGeocodingResult = {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: "OK" | "ZERO_RESULTS";
+};
 
 const formSubmitHandler = (event: Event) => {
     event.preventDefault();
     const input = formInput.value;
+
+    axios
+        .get<GoogleGeocodingResult>(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+                input
+            )}&key=${API_KEY}
+`
+        )
+        .then((response) => {
+            if (response.data.status !== "OK") {
+                throw new Error("Could not fetch results");
+            }
+            const coordinates = response.data.results[0].geometry.location;
+        })
+        .catch((err) => {
+            alert(err.message);
+        });
 };
 
 formElement.addEventListener("submit", formSubmitHandler);
